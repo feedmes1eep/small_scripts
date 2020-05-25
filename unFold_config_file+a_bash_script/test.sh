38438e8982ad808b3e303d1a143806dde8409ee0
feedmes1eep <YahadaKu@gmail.com>
2020-05-25 18:40:42 +0500

    fix

--- unFold_config_file+a_bash_script/test.sh
+++ unFold_config_file+a_bash_script/test.sh
@@ -6,7 +6,7 @@ mkdir $Bname/Transparent/
 python config-file-creation.py
 mv -fv *ini *png $Bname
 mv -fv $Bname/*ini $Bname/Transparent/
-cp -fv $Bname ~/Documents/Rainmeter/Skins/unFold/
+cp -rfv $Bname ~/Documents/Rainmeter/Skins/unFold/
 mv -fv $Bname More-Icons-unFold-Rainmeter/
 mv -fv More-Icons-unFold-Rainmeter/$Bname/*png More-Icons-unFold-Rainmeter/Buttons/Transparent
 cd ~/Documents/Rainmeter/Skins/unFold/
