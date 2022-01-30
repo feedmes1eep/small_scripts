fc0d129de11297c3e421ebf4814785371c5a2fa3
FeedMeS1eep <feedmesleep@protonmail.com>
2022-01-30 11:57:16 +0500

    Slight change to pwdgen script
    Added function to copy the generated password to the clipboard.

--- python_scripts/pwdgen.py
+++ python_scripts/pwdgen.py
@@ -1,7 +1,10 @@
 import os, random, string
+import pyperclip
 
 length = 15
 chars = string.ascii_letters + string.digits + '!@#$%^&*()'
 random.seed = (os.urandom(1024))
 
-print (''.join(random.choice(chars) for i in range(length)))
+ps = (''.join(random.choice(chars) for i in range(length)))
+pyperclip.copy(ps)
+print(f'{ps} is copied to your clipboard')
